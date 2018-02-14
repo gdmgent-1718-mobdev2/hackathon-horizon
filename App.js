acbe315a4f6442403a77dc1d6a08e56159f789de
simodecl <siodecl@student.arteveldehs.be>
2018-02-14 10:22:58 +0100

    Firebase init

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container style={styles.container}>
+        <FormLabel>Name</FormLabel>
+        <FormInput onChangeText={someFunction}/>
+        <FormValidationMessage>Error message</FormValidationMessage>
+      </Container>   
     );
   }
 }
