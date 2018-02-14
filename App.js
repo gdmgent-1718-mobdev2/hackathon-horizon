8d2d12fca098fdfd881734e170e2495be3a2f4da
simodecl <siodecl@student.arteveldehs.be>
2018-02-14 20:20:52 +0100

    ListView parkdata

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ListView } from 'react-native';
 import { FormLabel, FormInput } from 'react-native-elements';
+import * as firebase from 'firebase';
+import firebaseConfig from './utils/firebaseConfig.json';
+
+firebase.initializeApp(firebaseConfig);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds,
+    };
+    //'snapshot' maken van de data uit database
+    this.database = firebase.database();     
+    this.rootRef = firebase.database().ref();
+    this.ref = this.rootRef.child("parks");
+       
+  }
+
+  listenForParks(ref) {
+    ref.on('value', (dataSnapshot) => {
+      var parks = [];
+      dataSnapshot.forEach((child) => {
+        parks.push(
+          child.val().name
+        );
+      });
+      console.warn(parks);
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(parks)
+      });
+    });
+  }
+  componentDidMount() {
+    // start listening for firebase updates
+    this.listenForParks(this.ref);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <FormLabel>Name</FormLabel>
-        <FormInput onChangeText={someFunction}/>
-        <FormValidationMessage>Error message</FormValidationMessage>
-      </View>   
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+      </View> 
     );
   }
 }
